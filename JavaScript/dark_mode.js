4e750ef054b3e6e9b733f0108512176468b3f402
PRITAM DAS <er.pritamdas22@gmail.com>
2022-01-05 19:03:33 +0530

    Updated

---
+++ JavaScript/dark_mode.js
@@ -0,0 +1,27 @@
+
+
+
+function dark()
+{
+    if(document.body.style.backgroundColor == "black")
+    {
+        document.body.style.backgroundColor = "white";
+        document.body.style.transition = "0.5s linear";
+        document.body.style.color = "black";
+    }
+    else
+    {
+        document.body.style.backgroundColor = "black";
+        document.body.style.transition = "0.5s linear";
+        document.body.style.color = "white";
+    }
+
+}
+
+
+
+
+
+
+
+
