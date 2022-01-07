36c55af16ace1641d0e174580fd3b48697e40ea2
PRITAM DAS <er.pritamdas22@gmail.com>
2022-01-08 00:37:58 +0530

    Updated

--- JavaScript/CSS-Basics.js
+++ JavaScript/CSS-Basics.js
@@ -3,69 +3,230 @@ function bgcolor() {
     document.getElementById("bg_color").style.display = "block";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
 function bgimage() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "block";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
 function bgrepeat() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "block";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
-function bgposition() {
+function bgnorepeat() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "block";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "block";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
-function bgsize() {
+function bgrepeatx() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "block";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "block";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
-function bgattachment() {
+function bgrepeaty() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "block";
-    document.getElementById("bg_").style.display = "none";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "block";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
-function bg() {
+function bgrepeatspace() {
     document.getElementById("bg_color").style.display = "none";
     document.getElementById("bg_image").style.display = "none";
     document.getElementById("bg_repeat").style.display = "none";
-    document.getElementById("bg_position").style.display = "none";
-    document.getElementById("bg_size").style.display = "none";
-    document.getElementById("bg_attachment").style.display = "none";
-    document.getElementById("bg_").style.display = "block";
-
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "block";
+    document.getElementById("bg_repeataround").style.display = "none";
 }
+function bgrepeataround() {
+    document.getElementById("bg_color").style.display = "none";
+    document.getElementById("bg_image").style.display = "none";
+    document.getElementById("bg_repeat").style.display = "none";
+    document.getElementById("bg_norepeat").style.display = "none";
+    document.getElementById("bg_repeatx").style.display = "none";
+    document.getElementById("bg_repeaty").style.display = "none";
+    document.getElementById("bg_repeatspace").style.display = "none";
+    document.getElementById("bg_repeataround").style.display = "block";
+}
+
+
+ //Background box 2
+
+ let bg2;
+ bg2=document.getElementsByClassName("b2")
+
+function bgpositionll()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==0)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionlb()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==1)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+
+ function bgpositionlt()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==2)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionrt()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==3)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionrc()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==4)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionct()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==5)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionpercentage()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==6)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+ function bgpositionpixel()
+ {
+     for(let i=0;i<bg2.length;i++)
+     {
+         if(i==7)
+         {
+            bg2[i].style.display = "block";
+         }
+         else
+         {
+            bg2[i].style.display = "none";
+         }
+          
+     }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
