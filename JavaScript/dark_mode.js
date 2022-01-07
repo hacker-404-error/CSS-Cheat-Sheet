6d2f1f15650e8145ae14a00280c70503e4951b7a
PRITAM DAS <er.pritamdas22@gmail.com>
2022-01-07 08:59:48 +0530

    Updated

--- JavaScript/dark_mode.js
+++ JavaScript/dark_mode.js
@@ -8,12 +8,14 @@ function dark()
         document.body.style.backgroundColor = "white";
         document.body.style.transition = "0.5s linear";
         document.body.style.color = "black";
+        document.getElementsByTagName('a').style.color = "white";
     }
     else
     {
         document.body.style.backgroundColor = "black";
         document.body.style.transition = "0.5s linear";
         document.body.style.color = "white";
+        document.getElementsByTagName('a').style.color = "white";
     }
 
 }
