887204f47321ff2a4fd6e7957d3b683dc555cf01
PRITAM DAS <er.pritamdas22@gmail.com>
2022-01-10 11:18:54 +0530

    Updated

--- JavaScript/dark_mode.js
+++ JavaScript/dark_mode.js
@@ -15,7 +15,7 @@ function dark()
     {
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
-        document.body.style.backgroundImage = "url('https://image.freepik.com/free-vector/abstract-black-texture-background-hexagon_206725-413.jpg')";
+        document.body.style.backgroundImage = "url('https://i.ibb.co/5Ym5n1D/blackbg.jpg')";
         // document.getElementsByTagName("a").style.color = "white";
         document.body.style.transition = "0.5s linear";
     }
