0be1f601a30831edbe4a75a1520fd18db9317fe9
PRITAM DAS <er.pritamdas22@gmail.com>
2022-11-13 19:13:02 +0530

    Updated

--- 1.CSS-Basics/basic.js
+++ 1.CSS-Basics/basic.js
@@ -282,39 +282,6 @@ function back_re_6_func() {
     }
   }
 }
-function back_re_7_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 6) {
-      bg_class_re[6].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_8_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 7) {
-      bg_class_re[7].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_9_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 8) {
-      bg_class_re[8].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
 
 //*******************************************************************
 // 5.Background-Position
