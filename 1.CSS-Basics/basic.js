642fa9d9742280a7e880f69a8048510e3e392d1c
PRITAM DAS <er.pritamdas22@gmail.com>
2022-11-14 23:20:40 +0530

    Updated

--- 1.CSS-Basics/basic.js
+++ 1.CSS-Basics/basic.js
@@ -284,6 +284,51 @@ function back_re_6_func() {
 }
 
 //*******************************************************************
+// 4.Background-Attachment
+// *********************************************************************
+var bg_class_at = document.getElementsByClassName("back-at-class");
+
+//display:none for all the result display
+for (let i = 0; i < bg_class_at.length; i++) {
+  bg_class_at[i].style.display = "none";
+}
+
+function back_at_1_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
+    if (i == 0) {
+      bg_class_at[0].style.display = "block";
+    } else {
+      bg_class_at[i].style.display = "none";
+    }
+  }
+}
+
+function back_at_2_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
+    if (i == 1) {
+      bg_class_at[1].style.display = "block";
+    } else {
+      bg_class_at[i].style.display = "none";
+    }
+  }
+}
+
+function back_at_3_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
+    if (i == 2) {
+      bg_class_at[2].style.display = "block";
+    } else {
+      bg_class_at[i].style.display = "none";
+    }
+  }
+}
+//*******************************************************************
 // 5.Background-Position
 // *********************************************************************
 var bg_class_po = document.getElementsByClassName("back-po-class");
--- 5.EXTRA/Temp/temp.js
+++ 5.EXTRA/Temp/temp.js
@@ -1,112 +1,45 @@
 //*******************************************************************
-// 3.Background-Repeat
+// 4.Background-Attachment
 // *********************************************************************
-var bg_class_re = document.getElementsByClassName("back-re-class");
+var bg_class_at = document.getElementsByClassName("back-at-class");
 
 //display:none for all the result display
-for (let i = 0; i < bg_class_re.length; i++) {
-  bg_class_re[i].style.display = "none";
+for (let i = 0; i < bg_class_at.length; i++) {
+  bg_class_at[i].style.display = "none";
 }
 
-function back_re_1_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
+function back_at_1_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
     if (i == 0) {
-      bg_class_re[0].style.display = "block";
+      bg_class_at[0].style.display = "block";
     } else {
-      bg_class_re[i].style.display = "none";
+      bg_class_at[i].style.display = "none";
     }
   }
 }
 
-function back_re_2_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
+function back_at_2_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
     if (i == 1) {
-      bg_class_re[1].style.display = "block";
+      bg_class_at[1].style.display = "block";
     } else {
-      bg_class_re[i].style.display = "none";
+      bg_class_at[i].style.display = "none";
     }
   }
 }
 
-function back_re_3_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
+function back_at_3_func() {
+  for (let i = 0; i < bg_class_at.length; i++) {
+    var bg_initial_at = document.getElementById("back-at-initial");
+    bg_initial_at.style.display = "none";
     if (i == 2) {
-      bg_class_re[2].style.display = "block";
+      bg_class_at[2].style.display = "block";
     } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-
-function back_re_4_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 3) {
-      bg_class_re[3].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_5_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 4) {
-      bg_class_re[4].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_6_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 5) {
-      bg_class_re[5].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_7_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 6) {
-      bg_class_re[6].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_8_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 7) {
-      bg_class_re[7].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
-    }
-  }
-}
-function back_re_9_func() {
-  for (let i = 0; i < bg_class_re.length; i++) {
-    var bg_initial_re = document.getElementById("back-re-initial");
-    bg_initial_re.style.display = "none";
-    if (i == 8) {
-      bg_class_re[8].style.display = "block";
-    } else {
-      bg_class_re[i].style.display = "none";
+      bg_class_at[i].style.display = "none";
     }
   }
 }
